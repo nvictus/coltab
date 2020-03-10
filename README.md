f60f7b3fb5947e96006ad4e9768d5d8cfbddaf36
Nezar Abdennur <nabdennur@gmail.com>
2020-03-10 12:03:54 -0400

    Working zarr backend

--- README.md
+++ README.md
@@ -9,41 +9,10 @@
 >>> store.addcol('foo', 'd', [1.1, 2.2, 3.3])
 >>> store.delcol('foo', 'c')
 >>> store.select('foo')
-   a  b    c    d
-0  1  4  7.0  1.1
-1  2  5  8.0  2.2
-2  3  6  9.0  3.3
+   a  b    d
+0  1  4  1.1
+1  2  5  2.2
+2  3  6  3.3
 store.drop_table('foo')
 store.close()
 ```
-
-<!-- Pillars of data architecture
-
-Take any directory-like mutable mapping for arrays, and view and manipulate it as a table (i.e. dataframe). That's it!
-
-```python
-
->>> import coltab
->>> import h5py
-
-# low-level API
->>> f = h5py.File('myfile.h5', 'r+')
->>> df = coltab.get(f['foo/bar'], fields=['a', 'b', 'c'], lo=10, hi=100)
->>> coltab.put(f['baz'].create_group('quux'), df, 
-... 	storage_options={'compression': 'gzip', 'compression_opts': 6, 'shuffle': True})
-
-# high-level API
->>> view = coltab.Table('h5py://myfile.h5::foo/bar', storage_options=**opts)  # dask dataframe?
->>> df = view.loc[10:100]
->>> df = view[:]
->>> coltab.to_table('h5py://myfile.h5::baz/quux', view)
-
-
->>> coltab.shuffle()
->>> coltab.sort()
->>> coltab.indexing.rlencode()
->>> coltab.indexing.sparse_index()
-
-
-```
- -->
--- coltab/backend.py
+++ coltab/backend.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function
-from pandas.api.types import is_categorical, is_categorical_dtype
+from pandas.api.types import is_categorical  # is_categorical_dtype
 import numpy as np
 import pandas as pd
 import h5py
+import zarr
 import six
-import cooler
 
 from .base import Backend
 
 
+class StorageError(Exception):
+    pass
+
+
 def resource_split(uri):
     try:
-        uri, data_path = uri.rsplit('::', 1)
+        uri, data_path = uri.rsplit("::", 1)
     except ValueError:
-        data_path = ''
+        data_path = ""
     try:
-        proto, uri = uri.split('://')
+        proto, uri = uri.split("://", 1)
     except ValueError:
-        proto = ''
+        proto = ""
     return proto, uri, data_path
 
 
@@ -28,27 +32,26 @@ class DictStore(Backend):
 
 class HDF5Store(Backend):
 
-    DEFAULT_H5OPTS = {
-        'compression': 'gzip',
-        'compression_opts': 6,
-        'shuffle': True
-    }
+    DEFAULT_H5OPTS = {"compression": "gzip", "compression_opts": 6, "shuffle": True}
 
     def __init__(self, store_uri, storage_options=None, **kwargs):
         """
         Parameters
         ----------
-        store_path : str
-            Path to HDF5 file
-        group_path : str
-
+        store_uri : str
+            URI to HDF5 file or group.
+        storage_options : dict
+            ``h5py.Dataset`` storage options.
+        **kwargs
+            Keyword args to pass ``h5py.open`` when used as a context manager.
+            Default is to open in append 'a' mode.
 
         """
-        _, store_path, group_path = resource_split(store_uri) 
+        _, store_path, group_path = resource_split(store_uri)
         self.store_path = store_path
-        self.group_path = group_path or '/'
+        self.group_path = group_path or "/"
         self.storage_options = storage_options or self.DEFAULT_H5OPTS
-        self.open_kws = kwargs or {'mode': 'a'}
+        self.open_kws = kwargs or {"mode": "a"}
         self._open = False
 
     def open(self, mode):
@@ -81,58 +84,91 @@ class HDF5Store(Backend):
     def __exit__(self, *exc_info):
         self.close()
 
-    def create_table(self, name, dtypes, data=None, init_size=0, max_size=None):
-        """Create a empty directory if one does not exist. 
+    def create_table(self, name, dtypes=None, data=None, init_size=0, max_size=None):
+        """Create a empty group if one does not exist.
         Initialize arrays for columns.
         """
+        if not self._open:
+            raise ValueError("Operation on closed file")
+
+        if name in self.grp and isinstance(self.grp[name], h5py.Dataset):
+            raise StorageError(
+                "Cannot create group '{}'. Dataset already exists.".format(name)
+            )
+
         grp = self.grp.require_group(name)
-        for colname in dtypes:
-            if colname in grp.keys():
-                raise ValueError(
-                    "Found existing key '{}' in group '{}'.".format(
-                        colname, name))
-            grp.create_dataset(
-                colname,
-                shape=(init_size,),
-                maxshape=(max_size,),
-                dtype=dtypes[colname],
-                **self.storage_options)
-    
+
+        if len(grp.keys()):
+            raise StorageError("Existing group '{}' is not empty.".format(name))
+
+        if dtypes is not None:
+            for colname in dtypes.keys():
+                grp.create_dataset(
+                    colname,
+                    shape=(init_size,),
+                    maxshape=(max_size,),
+                    dtype=dtypes[colname],
+                    **self.storage_options
+                )
+        elif data is not None:
+            for colname in data.keys():
+                self.addcol(name, colname, data[colname])
+
     def drop_table(self, name):
-        """Remove the table's arrays and directory."""
+        """Remove the table's arrays and group."""
+        if not self._open:
+            raise ValueError("Operation on closed file")
+
         grp = self.grp[name]
-        if (isinstance(grp, h5py.Group) and 
-                all(isinstance(k, h5py.Dataset) for k in grp.values())):
-            tab_name = grp.name
-            if tab_name == '/':
+        if isinstance(grp, h5py.Group) and all(
+            isinstance(k, h5py.Dataset) for k in grp.values()
+        ):
+            table_name = grp.name
+            if table_name == "/":
                 for colname in self.grp.keys():
                     self.delcol(grp, colname)
             else:
                 parent = grp.parent
                 del parent[name]
-    
+
     def append(self, name, chunk):
+        if not self._open:
+            raise ValueError("Operation on closed file")
         assert set(chunk.keys()) == set(self.grp[name].keys())
         first = list(self.grp[name].keys())[0]
         self._put(name, chunk, len(self.grp[name][first]))
-    
+
     def addcol(self, name, colname, arr, dtype=None, pos=None):
-        """Append a column to the table. 
+        """Append a column to the table.
         Must have the same length as the other columns."""
+        if not self._open:
+            raise ValueError("Operation on closed file")
         d = {colname: arr}
         self._put(name, d, 0)
-    
+
     def delcol(self, name, colname):
         """Remove a column from a table."""
-        if colname in self.grp.keys():
+        if not self._open:
+            raise ValueError("Operation on closed file")
+        if colname in self.grp[name].keys():
             del self.grp[name][colname]
 
     def select(self, name, lo=0, hi=None, colnames=None):
-        """Select a range of values from a table, and 
+        """Select a range of values from a table, and
         optionally, a subset of columns.
         TODO: replace with .loc[]/.iloc[] or __getitem__?
         """
+        if not self._open:
+            raise ValueError("Operation on closed file")
         return self._get(name, lo, hi, colnames)
+
+    def tree(self):
+        if self._open:
+            import zarr.util
+
+            return zarr.util.TreeViewer(self.grp)
+        else:
+            raise ValueError("Operation on closed file")
 
     def _normalize_column(self, data, coerce_dtype=None, store_categories=True):
         """
@@ -167,7 +203,7 @@ class HDF5Store(Backend):
 
         elif data.dtype in (object, str, bytes):
             data = np.asarray(data)
-            dtype = np.dtype('S')
+            dtype = np.dtype("S")
             array = np.array(data, dtype=dtype)
             fillvalue = None
 
@@ -194,9 +230,7 @@ class HDF5Store(Backend):
 
             x = np.asarray(chunk[name])
 
-            data, dtype, fillvalue = self._normalize_column(
-                x, 
-                x.dtype)
+            data, dtype, fillvalue = self._normalize_column(x, x.dtype)
 
             if name in grp.keys():
                 dset = grp[name]
@@ -207,29 +241,31 @@ class HDF5Store(Backend):
 
                 try:
                     dset = grp.create_dataset(
-                        name, 
-                        shape=(hi,), 
+                        name,
+                        shape=(hi,),
                         maxshape=(None,),
-                        dtype=dtype, 
+                        dtype=dtype,
                         data=data,
                         fillvalue=fillvalue,
-                        **self.storage_options)
+                        **self.storage_options
+                    )
                 except ValueError:
                     enum_dict = h5py.check_dtype(enum=dtype)
                     if enum_dict is not None:
                         # enum definition too big for object header
                         # store as an attribute instead
                         dset = grp.create_dataset(
-                            name, 
-                            shape=(hi,), 
+                            name,
+                            shape=(hi,),
                             maxshape=(None,),
-                            dtype=int_dtype, 
+                            dtype=np.int64,
                             data=data,
                             fillvalue=fillvalue,
-                            **self.storage_options)
-                        dset.attrs['categories'] = sorted(
-                            enum_dict, 
-                            key=enum_dict.__getitem__)
+                            **self.storage_options
+                        )
+                        dset.attrs["categories"] = sorted(
+                            enum_dict, key=enum_dict.__getitem__
+                        )
                     else:
                         raise
         grp.file.flush()
@@ -246,23 +282,22 @@ class HDF5Store(Backend):
         data = {}
         for name in colnames:
             dset = grp[name]
-            
+
             categories = None
             if decode_enum:
                 enum_dict = h5py.check_dtype(enum=dset.dtype)
                 if enum_dict is not None:
                     categories = sorted(enum_dict, key=enum_dict.__getitem__)
-                elif 'categories' in dset.attrs:
-                    categories = dset.attrs['categories'].astype('U')
-                
+                elif "categories" in dset.attrs:
+                    categories = dset.attrs["categories"].astype("U")
+
             if categories is not None:
                 data[name] = pd.Categorical.from_codes(
-                    dset[lo:hi],
-                    categories,
-                    ordered=True)
+                    dset[lo:hi], categories, ordered=True
+                )
             elif dset.dtype.type == np.string_:
-                data[name] = dset[lo:hi].astype('U')
-            else: 
+                data[name] = dset[lo:hi].astype("U")
+            else:
                 # vlen_dtype = h5py.check_dtype(vlen=dset.dtype)
                 # no need to do this, vlen arrays automatically come out as object
                 data[name] = dset[lo:hi]
@@ -276,30 +311,248 @@ class HDF5Store(Backend):
             index = None
 
         if series:
-            return pd.Series(
-                data[colnames[0]],
-                index=index,
-                name=colnames[0])
+            return pd.Series(data[colnames[0]], index=index, name=colnames[0])
         else:
-            return pd.DataFrame(
-                data,
-                columns=colnames,
-                index=index)
+            return pd.DataFrame(data, columns=colnames, index=index)
 
 
 class ZarrStore(Backend):
-    def __init__(self, store_path, group_path, **kwargs):
+
+    DEFAULT_ZOPTS = {}
+
+    def __init__(self, store_uri, consolidated=False, storage_options=None, **kwargs):
+        _, store_path, group_path = resource_split(store_uri)
         self.store_path = store_path
         self.group_path = group_path
+        self.storage_options = storage_options or self.DEFAULT_ZOPTS
         self.open_kws = kwargs
-        self.grp = zarr.open_group(self.store_path, **self.open_kws)[self.group_path]
+        self._open = False
 
     def open(self, mode):
-        return zarr.open_group(self.store_path, mode)[self.group_path]
+        """Opens the store. Idempotent."""
+        if not self._open:
+            self.grp = zarr.open_group(self.store_path, mode)[self.group_path]
+            self._open = True
+
+    def close(self):
+        """Closes the store. Idempotent."""
+        if self._open:
+            self._open = False
 
     def __enter__(self):
-        return self.grp
+        self.open(**self.open_kws)
+        return self
 
     def __exit__(self, *exc_info):
-        pass
+        self.close()
 
+    def create_table(self, name, dtypes=None, data=None, init_size=0):
+        """Create a empty group if one does not exist.
+        Initialize arrays for columns.
+        """
+        if not self._open:
+            raise ValueError("Operation on closed file")
+
+        if name in self.grp and isinstance(self.grp[name], zarr.Array):
+            raise StorageError(
+                "Cannot create group '{}'. Array already exists.".format(name)
+            )
+
+        grp = self.grp.require_group(name)
+
+        if len(grp.keys()):
+            raise StorageError("Existing group '{}' is not empty.".format(name))
+
+        if dtypes is not None:
+            for colname in dtypes.keys():
+                grp.create_dataset(
+                    colname,
+                    shape=(init_size,),
+                    dtype=dtypes[colname],
+                    **self.storage_options
+                )
+        elif data is not None:
+            for colname in data.keys():
+                self.addcol(name, colname, data[colname])
+
+    def drop_table(self, name):
+        """Remove the table's arrays and group."""
+        if not self._open:
+            raise ValueError("Operation on closed file")
+
+        table_grp = self.grp[name]
+        if isinstance(table_grp, zarr.Group) and all(
+            isinstance(k, zarr.Array) for k in table_grp.values()
+        ):
+            table_name = table_grp.name
+            if table_name == "/":
+                for colname in self.grp.keys():
+                    self.delcol(table_grp, colname)
+            else:
+                del self.grp[name]
+
+    def append(self, name, chunk):
+        if not self._open:
+            raise ValueError("Operation on closed file")
+        assert set(chunk.keys()) == set(self.grp[name].keys())
+        first = list(self.grp[name].keys())[0]
+        self._put(name, chunk, len(self.grp[name][first]))
+
+    def addcol(self, name, colname, arr, dtype=None, pos=None):
+        """Append a column to the table.
+        Must have the same length as the other columns."""
+        if not self._open:
+            raise ValueError("Operation on closed file")
+        d = {colname: arr}
+        self._put(name, d, 0)
+
+    def delcol(self, name, colname):
+        """Remove a column from a table."""
+        if not self._open:
+            raise ValueError("Operation on closed file")
+        if colname in self.grp[name].keys():
+            del self.grp[name][colname]
+
+    def select(self, name, lo=0, hi=None, colnames=None):
+        """Select a range of values from a table, and
+        optionally, a subset of columns.
+        TODO: replace with .loc[]/.iloc[] or __getitem__?
+        """
+        if not self._open:
+            raise ValueError("Operation on closed file")
+        return self._get(name, lo, hi, colnames)
+
+    def tree(self):
+        if self._open:
+            import zarr.util
+
+            return zarr.util.TreeViewer(self.grp)
+        else:
+            raise ValueError("Operation on closed file")
+
+    def _normalize_column(self, data, coerce_dtype=None, store_categories=True):
+        """
+        Make column suitable for HDF5 storage.
+
+        * numerical and boolean types map as they should
+        * bytes colunms map to type S arrays -- they won't roundtrip
+        * str or object columns map to type S arrays
+        * categorical columns:
+            * make an ENUM type (may end up being too large for HDF5 to accept)
+
+        """
+        if coerce_dtype is not None:
+            coerce_dtype = np.dtype(coerce_dtype)
+
+        if np.isscalar(data):
+            array = np.array([data], dtype=coerce_dtype)
+            dtype = data.dtype
+            fillvalue = None
+
+        elif is_categorical(data):
+            if store_categories:
+                cats = data.cat.categories
+                enum_dict = dict(zip(cats, range(len(cats))))
+                array = data.cat.codes
+                dtype = h5py.special_dtype(enum=(array.dtype, enum_dict))
+                fillvalue = -1
+            else:
+                array = data.cat.codes
+                dtype = coerce_dtype or array.dtype
+                fillvalue = -1
+
+        elif data.dtype in (object, str, bytes):
+            data = np.asarray(data)
+            dtype = np.dtype("S")
+            array = np.array(data, dtype=dtype)
+            fillvalue = None
+
+        else:
+            array = np.asarray(data)
+            dtype = data.dtype
+            fillvalue = None
+
+        return array, dtype, fillvalue
+
+    def _put(self, name, chunk, row_offset):
+        """Append columns from a dataframe to the columns in the table.
+        All input columns must already exist in the table."""
+        grp = self.grp[name]
+        lo = row_offset
+        if isinstance(chunk, pd.Series):
+            chunk = chunk.to_frame()
+            n_rows = len(chunk)
+        else:
+            n_rows = len(chunk[next(iter(chunk.keys()))])
+        hi = lo + n_rows
+
+        for name in chunk.keys():
+
+            x = np.asarray(chunk[name])
+
+            data, dtype, fillvalue = self._normalize_column(x, x.dtype)
+
+            if name in grp.keys():
+                dset = grp[name]
+                if hi > len(dset):
+                    dset.resize((hi,))
+                dset[lo:hi] = data
+            else:
+                try:
+                    enum_dict = h5py.check_dtype(enum=dtype)
+                except AttributeError:
+                    enum_dict = None
+                dset = grp.create_dataset(
+                    name,
+                    shape=(hi,),
+                    dtype=dtype,
+                    data=data,
+                    fillvalue=fillvalue,
+                    **self.storage_options
+                )
+                if enum_dict is not None:
+                    # store enum dictionary as attribute
+                    dset.attrs["categories"] = sorted(
+                        enum_dict, key=enum_dict.__getitem__
+                    )
+
+    def _get(self, name, lo=0, hi=None, colnames=None, decode_enum=True, as_dict=False):
+        grp = self.grp[name]
+        series = False
+        if colnames is None:
+            colnames = list(grp.keys())
+        elif isinstance(colnames, six.string_types):
+            colnames = [colnames]
+            series = True
+
+        data = {}
+        for name in colnames:
+            dset = grp[name]
+
+            categories = None
+            if decode_enum and "categories" in dset.attrs:
+                categories = dset.attrs["categories"].astype("U")
+
+            if categories is not None:
+                data[name] = pd.Categorical.from_codes(
+                    dset[lo:hi], categories, ordered=True
+                )
+            elif dset.dtype.type == np.string_:
+                data[name] = dset[lo:hi].astype("U")
+            else:
+                # vlen_dtype = h5py.check_dtype(vlen=dset.dtype)
+                # no need to do this, vlen arrays automatically come out as object
+                data[name] = dset[lo:hi]
+
+        if as_dict:
+            return data
+
+        if data and lo is not None:
+            index = np.arange(lo, lo + len(next(iter(data.values()))))
+        else:
+            index = None
+
+        if series:
+            return pd.Series(data[colnames[0]], index=index, name=colnames[0])
+        else:
+            return pd.DataFrame(data, columns=colnames, index=index)
