14c3f83a495dd8abfa39a8aaa51e716c481bbcce
Nezar Abdennur <nabdennur@gmail.com>
2020-03-10 14:11:50 -0400

    Some refactoring

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,4 +1,3 @@
-from __future__ import division, print_function
 import tempfile
 import os.path as op
 import os
@@ -12,15 +11,15 @@ import pytest
 
 testdir = os.path.dirname(os.path.realpath(__file__))
 tmp = tempfile.gettempdir()
-EXAMPLE_PATH = os.path.join(tmp, 'test.h5')
+EXAMPLE_PATH = os.path.join(tmp, "test.h5")
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def store(request):
-    
     def teardown():
         if op.exists(EXAMPLE_PATH):
             os.remove(EXAMPLE_PATH)
+
     request.addfinalizer(teardown)
 
     return HDF5Store(EXAMPLE_PATH)
@@ -28,42 +27,40 @@ def store(request):
 
 def test_create_table(store):
     with store:
-        tab = store.create_table(
-            'baz', 
-            {'A': int, 'B': int, 'C':float})
+        store.create_table("baz", {"A": int, "B": int, "C": float})
 
-        assert 'baz' in store.grp.keys()
+        assert "baz" in store.grp.keys()
 
 
 def test_append(store):
     in_df = pd.DataFrame({
-        'A': [1,2,3], 
-        'B': [4.1, 5.1, 6.1], 
-        'C': [4.1, 5.1, 6.1]
+        "A": [1, 2, 3],
+        "B": [4.1, 5.1, 6.1],
+        "C": [4.1, 5.1, 6.1]
     })
     with store:
-        store.append('baz', in_df)
-        out_df = store.select('baz')
+        store.append("baz", in_df)
+        out_df = store.select("baz")
     assert len(in_df) == len(out_df)
     assert np.all(in_df.columns == out_df.columns)
 
 
 def test_append_col(store):
     with store:
-        store.addcol('baz', 'E', [6,7,8])
-        df = store.select('baz')
-    assert 'E' in df.columns
+        store.addcol("baz", "E", [6, 7, 8])
+        df = store.select("baz")
+    assert "E" in df.columns
 
 
 def test_remove_col(store):
     with store:
-        store.delcol('baz', 'D')
-        df = store.select('baz')
-    assert 'D' not in df.columns
+        store.delcol("baz", "D")
+        df = store.select("baz")
+    assert "D" not in df.columns
 
 
 def test_drop_table(store):
     with store:
-        store.drop_table('baz')
+        store.drop_table("baz")
         with pytest.raises(KeyError):
-            store.select('baz')
+            store.select("baz")
